568c52d362c70d0b6268a3f1c3ec7255d29b9bae
ArefHebri <arefhebri@gmail.com>
2019-11-15 13:02:33 +0330

    Updates

---
+++ precache-manifest.95de3f6530e7ac659055a281a24d2fc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b30c90af722a9253035308a6f9f3a3",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "10aaa807b921fa1c59a4",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8b8303d82dfe51eacfa9",
+    "url": "/robofriends/static/css/main.3c5881eb.chunk.css"
+  },
+  {
+    "revision": "10aaa807b921fa1c59a4",
+    "url": "/robofriends/static/js/2.36d0c395.chunk.js"
+  },
+  {
+    "revision": "8b8303d82dfe51eacfa9",
+    "url": "/robofriends/static/js/main.23fc2393.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
